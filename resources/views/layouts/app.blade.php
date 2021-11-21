66e7a8ac0c115b1eac8b6045176712e6e3a89b7a
zahra <shainy.i76@gmail.com>
2021-11-21 10:28:33 +0330

    front web reserve

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body class="text-right bg-dark">
+<body class="text-right bg-light">
 <div id="app">
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
         <div class="container">
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,8 +5,25 @@
         <img src="https://cdn.alibaba.ir/h/desktop/assets/images/hero/hero.jpg-c519cbc5.jpg" alt="Bus"
              class="" height="320">
     </div>
-    <div class="position-absolute d-flex justify-content-center w-100" style="top:300px">
-        <div class=" bg-white rounded w-75 p-4">
+    <div class="position-absolute d-flex justify-content-center w-100" style="top:310px">
+        <div class=" bg-white rounded w-75 border">
+            <div class="border-bottom p-4 text-primary h4 text-center">
+                {{__('bus ticket')}}
+            </div>
+            <form class="p-4">
+                <div class="d-flex">
+                    <div class="input-group input-group-lg col-5 p-0 " dir="ltr">
+                        <input dir={{__('direction')}} type="text" class="form-control" placeholder={{__('destination')}} >
+                        <input dir={{__('direction')}} type="text" class="form-control" placeholder={{__('origin')}} >
+                    </div>
+                    <div class="col-5 px-2">
+                        <input type="text" class="form-control form-control-lg" placeholder="{{__('date of departure')}}" aria-label="Username" aria-describedby="basic-addon1">
+                    </div>
+                    <div class="col-2 p-0">
+                        <button class="btn btn-lg w-100" style="background-color: #FDB93C; border-color: #E3A535">{{__('search')}}</button>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 
